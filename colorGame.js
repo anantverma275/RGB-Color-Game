2a108feab5fa435e1aa3d5fac0ee2c8b66c27ca5
Anant Verma <anantverma275@users.noreply.github.com>
2019-08-24 01:00:38 +0530

    firefox error and several bugs fixed

--- colorGame.js
+++ colorGame.js
@@ -7,7 +7,7 @@ var bar = document.querySelector(".bar");
 
 function setColors(){
 	for(var i=0; i<squares.length;i++){
-		squares[i].style.background = "rgb(" + Math.random()*256 + ", " + Math.random()*256 + ", " + Math.random()*256 + ")";
+		squares[i].style.backgroundColor = "rgb(" + Math.random()*256 + ", " + Math.random()*256 + ", " + Math.random()*256 + ")";
 	}
 }
 
@@ -21,31 +21,31 @@ setColors();
 
 buttonRetry.addEventListener("click", function(){
 	setColors();
-	bar.style.background = "#1365a8";
-	colorCode = (squares[getRandomNumber(squares.length)].style.background);
+	bar.style.backgroundColor = "#1365a8";
+	colorCode = (squares[getRandomNumber(squares.length)].style.backgroundColor);
 	displayStatus.textContent = "";
 	head.textContent = colorCode.toUpperCase();
 	if(buttonRetry.textContent == "Play Again")
 		buttonRetry.textContent = "New Colors";
 });
 
-colorCode = (squares[getRandomNumber(squares.length)].style.background);
+colorCode = (squares[getRandomNumber(squares.length)].style.backgroundColor);
 head.textContent = colorCode.toUpperCase();
 
 for(var i=0; i<squares.length;i++){
 	squares[i].addEventListener("click", function(){
-		if(this.style.background==colorCode){
-			bar.style.background = colorCode;
+		if(this.style.backgroundColor==colorCode){
+			bar.style.backgroundColor = colorCode;
 			// alert("Correct");
 			displayStatus.textContent = "Correct!";
 			for(var k = 0; k<squares.length;k++)
-				squares[k].style.background = colorCode;
+				squares[k].style.backgroundColor = colorCode;
 			buttonRetry.textContent = "Play Again";
 		}
 		else {
 			// alert("Incorrect");
 			displayStatus.textContent = "Try Again!"
-			this.style.background = "#212121";
+			this.style.backgroundColor = "#212121";
 		}
 	});
 };
